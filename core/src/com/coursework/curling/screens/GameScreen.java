ebc1114adcf259461b1eb7b7b86853dc93ed25c4
GodFaterFedor <fedorotec7@gmail.com>
2017-02-22 19:58:29 +0300

    Initial commit

---
+++ core/src/com/coursework/curling/screens/GameScreen.java
@@ -0,0 +1,25 @@
+package com.coursework.curling.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+
+
+public class GameScreen extends ScreenAdapter {
+
+
+    private void update (float delta) {
+
+    }
+
+    private void draw (float delta) {
+
+    }
+
+    @Override
+    public void render (float delta) {
+
+        update(delta);
+        draw(delta);
+
+    }
+}
---
+++ desktop/src/com/coursework/curling/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coursework.curling.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coursework.curling.Curling;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Curling(), config);
+	}
+}
