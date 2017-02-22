1ece43764a8f94171bf0830d85472ef906298c42
GodFaterFedor <fedorotec7@gmail.com>
2017-02-22 23:21:02 +0300

    camera setup

--- core/src/com/coursework/curling/screens/GameScreen.java
+++ core/src/com/coursework/curling/screens/GameScreen.java
@@ -1,25 +1,79 @@
 package com.coursework.curling.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.coursework.curling.Curling;
 
 
+public class GameScreen implements Screen {
 
-public class GameScreen extends ScreenAdapter {
+    private Curling game;
+    private Texture car;
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
+    public GameScreen (Curling game) {
 
-    private void update (float delta) {
-
+        this.game = game;
+        car = new Texture("back.jpg");
+        camera = new OrthographicCamera();
+        viewport = new FillViewport(450 , 4500, camera);
+        viewport.apply();
     }
 
-    private void draw (float delta) {
+
+    @Override
+    public void show() {
+        camera.position.set(viewport.getWorldWidth() / 2 , camera.viewportHeight / 2 , 0);
 
     }
 
     @Override
-    public void render (float delta) {
+    public void render(float delta) {
 
-        update(delta);
-        draw(delta);
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(car, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        car.dispose();
     }
 }
